8572c648b4e99200c69096e42f65f538f6d6e3ed
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-10-27 11:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function solve()
+	{
+		const select = document.querySelector("select");
+		const selectedvalue = select.value;
+		let options =select.children;
+		for(let i=0;i<options.length;i++)
+			{
+				if(options[i].value===selectedvalue)
+				{
+					options[i].remove();
+					break;
+				}
+			}
+	}
+
+const button = document.getElementById("fe");
+button.addEventListener("click",solve)
